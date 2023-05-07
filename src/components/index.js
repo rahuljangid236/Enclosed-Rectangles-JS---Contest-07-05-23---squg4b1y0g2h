bad2fc1cf8613d29d903cfa3069d9dd5bae84c11
Rahul Jangid <39289586+rahuljangid236@users.noreply.github.com>
2023-05-07 12:24:11 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+//rec = {
+//	top: '25px',
+//	left: '96px',
+//	width: '64px',
+//	height: '96px',
+//      children: []
+//}
+
+function check(rec1,rec2){
+	//write your code
+}
+
+module.exports = check;
